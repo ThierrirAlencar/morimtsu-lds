919e1cf6118b359639d2a0f54237c13507b90c5f
ciringa <thierrird957468@gmail.com>
2025-10-21 12:37:14 -0300

    feat:fix student.module not linked

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaService } from './infra/database/prisma.service';
 import { AuthModule } from './app/modules/auth.module';
 import { baseController } from './app/base.controller';
 import { classModule } from './app/modules/class.module';
+import { studentModule } from './app/modules/student.module';
 
 @Module({
-  imports: [UserModule,AuthModule,classModule],
+  imports: [UserModule,AuthModule,classModule,studentModule],
   controllers: [baseController],
   providers: [PrismaService],
 })

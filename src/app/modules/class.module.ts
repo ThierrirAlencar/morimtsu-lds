4caf26ab7cf766adc8197590528e32a7e4c734fa
ciringa <thierrird957468@gmail.com>
2025-10-21 10:27:08 -0300

    fix: controller not added

--- src/app/modules/class.module.ts
+++ src/app/modules/class.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { classService } from "src/core/services/class.service";
 import { PrismaService } from "src/infra/database/prisma.service";
+import { classController } from "../controllers/class.controller";
 
 @Module({
+    controllers:[classController]
     providers:[classService,PrismaService]
 })
 export class classModule{

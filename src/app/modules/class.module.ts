969f39dde410b8832623c8c4a181f09628138195
ciringa <thierrird957468@gmail.com>
2025-10-21 10:27:26 -0300

    fix: some bug

--- src/app/modules/class.module.ts
+++ src/app/modules/class.module.ts
@@ -4,7 +4,7 @@ import { PrismaService } from "src/infra/database/prisma.service";
 import { classController } from "../controllers/class.controller";
 
 @Module({
-    controllers:[classController]
+    controllers:[classController],
     providers:[classService,PrismaService]
 })
 export class classModule{

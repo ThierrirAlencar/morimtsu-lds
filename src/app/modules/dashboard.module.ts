feed14d4a33a8118af1472c63ee0c5c1d16c4ae6
ciringa <thierrird957468@gmail.com>
2025-12-10 13:52:59 -0300

    [#]fix: dashboard prismaService import

--- src/app/modules/dashboard.module.ts
+++ src/app/modules/dashboard.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { dashboardController } from "../controllers/dashboard.controller";
 import { DataService } from "src/core/services/data.service";
+import { PrismaService } from "src/infra/database/prisma.service";
 
 
 
 @Module({
     controllers:[dashboardController],
-    providers:[DataService],
+    providers:[DataService, PrismaService],
 })
 export class dashboardModule{}
